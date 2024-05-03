c5512330e3f73fcc7c04aeedfc6f7fa05def7640
madan-dinata <madanmr48@gmail.com>
2024-05-03 09:18:52 +0700

    Initial Commit

---
+++ connectWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  connectWatch
+//
+//  Created by Muhammad Ramadhan on 03/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ connectWatch/connectWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectWatchApp.swift
+//  connectWatch
+//
+//  Created by Muhammad Ramadhan on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct connectWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
