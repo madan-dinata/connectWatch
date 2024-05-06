f1da09ae5661e75df401e5c32f46e1685b322ba9
madan-dinata <madanmr48@gmail.com>
2024-05-06 11:18:54 +0700

    first commit

---
+++ AppleWatch Watch App/AppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchApp.swift
+//  AppleWatch Watch App
+//
+//  Created by Muhammad Ramadhan on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
